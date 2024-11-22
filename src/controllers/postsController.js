def0f1b0651679287ba927f537a0731e48bb365a
Alexandre Sousa Silva <alexandresousasilva@icloud.com>
2024-11-22 20:00:22 -0300

    feat: add endpoint to upload image post

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,5 @@
 import { getAllPostsFromDB, createPostInDB } from '../models/postsModel.js';
+import fs from 'fs';
 
 export async function getAllPosts(_req, res) {
   try {
@@ -14,6 +15,23 @@ export async function createPost(req, res) {
   const newPost = req.body;
   try {
     const createdPost = await createPostInDB(newPost);
+    res.status(201).json(createdPost);
+  } catch (error) {
+    console.error('Erro ao criar post ->', error.message);
+    res.status(500).json({ error: 'Erro ao criar post' });
+  }
+}
+
+export async function imageUpload(req, res) {
+  const newPost = {
+    description: '',
+    imageUrl: req.file.originalname,
+    alt: '',
+  };
+  try {
+    const createdPost = await createPostInDB(newPost);
+    const updatedImage = `uploads/${createdPost.insertedId}.jpg`;
+    fs.renameSync(req.file.path, updatedImage);
     res.status(201).json(createdPost);
   } catch (error) {
     console.error('Erro ao criar post ->', error.message);
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,9 +1,17 @@
 // Responsabilidade: ouvir a requisição e direcionar para o caminho correspondente (controller)
-import { createPost, getAllPosts } from '../controllers/postsController.js';
+import multer from 'multer';
+import {
+  createPost,
+  getAllPosts,
+  imageUpload,
+} from '../controllers/postsController.js';
+
+const upload = multer({ dest: './uploads' });
 
 const routes = (app) => {
   app.get('/posts', getAllPosts);
   app.post('/posts', createPost);
+  app.post('/posts/images', upload.single('image'), imageUpload);
 };
 
 export default routes;
