3380a0301e7dc19cbb21d9e2d6686fd372ec8a8c
Alexandre Sousa Silva <alexandresousasilva@icloud.com>
2024-11-22 20:38:22 -0300

    feat: set `uploads` folder  as public to exposed content

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ import routes from './src/routes/postsRoutes.js';
 
 const app = express();
 const port = 3000;
+app.use(express.json());
+app.use(express.static('uploads'));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
-app.use(express.json());
 
 routes(app);
