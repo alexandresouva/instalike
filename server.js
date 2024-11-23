62fb315671e343a34775674c9be2bf8c09ee2956
Alexandre Sousa Silva <alexandresousasilva@icloud.com>
2024-11-22 21:34:24 -0300

    feat: configure CORS to front-end

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 // Responsabilidade: instanciar e controlar a configuração do servidor
 import express from 'express';
 import routes from './src/routes/postsRoutes.js';
+import cors from 'cors';
 
 const app = express();
+const corsOptions = {
+  origin: 'http://localhost:8000',
+  optionsSuccessStatus: 200,
+};
 const port = 3000;
+
 app.use(express.json());
 app.use(express.static('uploads'));
+app.use(cors(corsOptions));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
